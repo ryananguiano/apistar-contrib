cfdfd457881d57a548998f335ae78d9d0e48b166
Ryan Anguiano <ryan.anguiano@gmail.com>
2018-05-18 22:43:03 -0400

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ryananguiano/apistar-contrib',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
