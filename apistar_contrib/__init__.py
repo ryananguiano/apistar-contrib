6e05b79859fcfbf3aa250afc0fa982f3613642c9
Ryan Anguiano <ryan.anguiano@gmail.com>
2018-05-18 22:42:39 -0400

    Remove default components

--- apistar_contrib/__init__.py
+++ apistar_contrib/__init__.py
@@ -4,14 +4,3 @@ __author__ = """Ryan Anguiano"""
 __email__ = 'ryan.anguiano@gmail.com'
 __version__ = '0.0.3'
 
-
-from apistar_contrib.sessions import SessionComponent
-
-commands = [
-
-]
-
-components = [
-    SessionComponent(),
-]
-
